e46697797fa5e0a26aee24865bba0ca3070e1b33
saurabh kumar <saurabhsattaf@gmail.com>
2025-12-20 00:28:01 +0530

    sg added

--- main.tf
+++ main.tf
@@ -15,3 +15,14 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg_name"
   location = "West Europe"
 }
+resource "azurerm_storage_account" "stg" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
